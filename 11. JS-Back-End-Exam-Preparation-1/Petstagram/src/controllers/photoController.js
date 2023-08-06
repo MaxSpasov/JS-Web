3ecad593f894a534affeed2dfedcc10bdb9c053f
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-08-06 12:31:52 +0300

    Submissions

---
+++ 11. JS-Back-End-Exam-Preparation-1/Petstagram/src/controllers/photoController.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+const photoManager = require('../managers/photoManager');
+const { getErrorMessage } = require('../utils/errorHelpers');
+
+router.get('/', async (req, res) => {
+    const photos = await photoManager.getAll().lean();
+
+    res.render('photos', { photos });
+});
+router.get('/create', (req, res) => {
+    res.render('photos/create');
+});
+
+router.post('/create', async (req, res) => {
+    const photoData = {
+        ...req.body,
+        owner: req.user._id,
+    };
+
+    try {
+        await photoManager.create(photoData);
+
+        res.redirect('/photos');
+    } catch (err) {
+        res.render('photos/create', { error: getErrorMessage(err) });
+    }
+});
+
+router.get('/:photoId/details', async (req, res) => {
+    const photoId = req.params.photoId;
+    const photo = await photoManager.getOne(photoId).lean();
+    const isOwner = req.user?._id == photo.owner._id;
+
+    res.render('photos/details', { photo, isOwner });
+});
+
+router.get('/:photoId/delete', async (req, res) => {
+    const photoId = req.params.photoId;
+
+    try {
+        await photoManager.delete(req.params.photoId);
+        
+        res.redirect('/photos');
+    } catch (err) {
+        res.render(`photos/details`, {error: 'Unsuccessful photo deletion'})
+    }
+});
+
+module.exports = router;
---
+++ 11. JS-Back-End-Exam-Preparation-1/Petstagram/src/managers/photoManager.js
@@ -0,0 +1,9 @@
+const Photo = require('../models/Photo');
+
+exports.getAll = () => Photo.find().populate('owner');
+
+exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
+
+exports.create = (photoData) => Photo.create(photoData);
+
+exports.delete = (photoId) => Photo.findByIdAndDelete(photoId);
